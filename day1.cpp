8cbd599d35fb8bda6557bf16f98afc7ed3adbebf
jacob-machado-bwtek <118930827+jacob-machado-bwtek@users.noreply.github.com>
2024-12-02 10:51:18 -0500

    cmake stuf

--- day1.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, Advent of Code 2024!" << std::endl;
-    return 0;
-}
---
+++ day1/day1.cpp
@@ -0,0 +1,8 @@
+#include "utils.h"
+
+int main() {
+    string swag = "swagger  ";
+    cout << swag << endl;
+    aoc::trim(swag);
+    cout << swag << endl;
+}
